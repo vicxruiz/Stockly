6552b2ba95bf9fe443c20bac844215321cdb94bf
victor <ruizdvictor@gmail.com>
2019-05-21 21:52:19 -0400

    first commit

---
+++ Stockly/Stockly/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stockly
+//
+//  Created by Victor  on 5/21/19.
+//  Copyright © 2019 com.Victor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
