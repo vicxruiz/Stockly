d1e3cc87244d5f51d441db77538496a8119373cc
victor <ruizdvictor@gmail.com>
2019-05-21 22:41:53 -0400

    updated onboarding logic

--- Stockly/Stockly/Controllers/OnboardingContentViewController.swift
+++ Stockly/Stockly/Controllers/OnboardingContentViewController.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+import UIKit
+
+class OnboardingContentViewController: UIViewController {
+    
+    //MARK: Properties
+    @IBOutlet weak var headingLabel: UILabel! {
+        didSet {
+            headingLabel.numberOfLines = 0
+        }
+    }
+    
+    @IBOutlet weak var subheadingLabel: UILabel! {
+        didSet {
+            subheadingLabel.numberOfLines = 0
+        }
+    }
+    
+    @IBOutlet weak var contentImageView: UIImageView!
+    
+    var index = 0
+    var heading = ""
+    var subHeading = ""
+    var imageFile = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        headingLabel.text = heading
+        subheadingLabel.text = subHeading
+        contentImageView.image = UIImage(named: imageFile)
+    }
+}
---
+++ Stockly/Stockly/Styling/Service.swift
@@ -0,0 +1,36 @@
+//
+//  Service.swift
+//  Stockly
+//
+//  Created by Victor  on 5/21/19.
+//  Copyright © 2019 com.Victor. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Service {
+    //Navigation
+    static let navigationBarTintColor = UIColor.blue
+    
+    //Button
+    static let buttonTitleColor = UIColor.white
+    static let buttonBackgroundColor = UIColor.white
+    static let buttonCornerRadius: CGFloat = 15
+    static let butttonTitleFont: CGFloat = 18
+    
+    //design == most of the app should have these colors
+    static let baseColor = UIColor.blue
+    static let darkBaseColor = UIColor.darkGray
+    static let lightBaseColor = UIColor.lightGray
+    
+    //user alerts
+    static func showAlert(on: UIViewController, style: UIAlertController.Style, title: String?, message: String?, actions: [UIAlertAction] = [ UIAlertAction(title: "OK", style: .default, handler: nil)], completion: (() -> Swift.Void)? = nil) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: style)
+        for action in actions {
+            alert.addAction(action)
+        }
+        on.present(alert, animated: true, completion: completion)
+    }
+    
+}
