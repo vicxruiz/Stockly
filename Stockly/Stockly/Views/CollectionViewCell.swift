bdcecafcd7ee0058a5b2e15cd849f3eae70d4dc8
victor <ruizdvictor@gmail.com>
2019-05-23 18:43:30 -0400

    added collection view cell

---
+++ Stockly/Stockly/Views/CollectionViewCell.swift
@@ -0,0 +1,36 @@
+//
+//  CollectionViewCell.swift
+//  Stockly
+//
+//  Created by Victor  on 5/23/19.
+//  Copyright © 2019 com.Victor. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+
+class MarketNewsCollectionViewCell: UICollectionViewCell {
+    
+}
+
+class SymbolNewsCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var headlineLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+}
+
+class StockTwitsCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var messagesLabel: UILabel!
+}
+
+
+
+
+
+
+
+
