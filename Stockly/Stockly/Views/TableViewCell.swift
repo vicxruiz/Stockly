85d2b1052b97ac5e24e4e5e0881cfe306f0bfc1d
victor <ruizdvictor@gmail.com>
2019-05-23 18:37:00 -0400

    added table view cell outlets and connected them to storyboard

---
+++ Stockly/Stockly/Views/TableViewCell.swift
@@ -0,0 +1,52 @@
+//
+//  TableViewCell.swift
+//  Stockly
+//
+//  Created by Victor  on 5/23/19.
+//  Copyright © 2019 com.Victor. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class KeyDataTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var keyDataKeyLabel: UILabel!
+    @IBOutlet weak var keyDataValueLabel: UILabel!
+}
+
+class StatsTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var statsKeyLabel: UILabel!
+    @IBOutlet weak var statsValueLabel: UILabel!
+}
+
+
+class DividendTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var dividendKeyLabel: UILabel!
+    @IBOutlet weak var dividendValueLabel: UILabel!
+}
+
+
+
+class TradeDataTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var tradeDataKeyLabel: UILabel!
+    @IBOutlet weak var tradeDataValueLabel: UILabel!
+    
+}
+
+class ShareStatsTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var shareStatsKeyLabel: UILabel!
+    @IBOutlet weak var shareStatsValueLabel: UILabel!
+    
+}
+
+
+class FinancialsTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var financialsKeyLabel: UILabel!
+    @IBOutlet weak var financialsValueLabel: UILabel!
+}
